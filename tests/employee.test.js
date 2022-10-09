0685b47238ec48cf813aaa636a78457a5544955b
Zac Smart <zac_chaz@outlook.com>
2022-10-10 10:57:47 +1100

    employee test added

---
+++ tests/employee.test.js
@@ -0,0 +1,13 @@
+const Employee = require('../lib/employee')
+describe("details", () => {
+    describe("init", () => {
+        it("return object ", () => {
+            const obj = new Employee();
+            expect(typeof (obj)).tobe("object");
+        })
+        describe("getName function", () => {
+            const obj = new Employee('Jimbob');
+            expect(obj.getName()).tobe('Jimbob');
+        })
+    })
+});
