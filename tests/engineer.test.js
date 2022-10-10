3c738833aa8ca56f96ca79036828f5eaae3299e0
Zac Smart <zac_chaz@outlook.com>
2022-10-10 11:33:27 +1100

    changed engineer test

--- tests/engineer.test.js
+++ tests/engineer.test.js
@@ -2,12 +2,13 @@ const Engineer = require('../lib/engineer')
 describe("details", () => {
     describe("init", () => {
         it("return object ", () => {
-            const obj = new Employee();
+            const obj = new Engineer();
             expect(typeof (obj)).tobe("object");
         })
-        describe("getName function", () => {
-            const obj = new Employee('Jimbob');
-            expect(obj.getName()).tobe('Jimbob');
+        describe("getGithub function", () => {
+            it("return Github name",()=>{
+            const obj = new Engineer('Antonio Freewater', '2', 'testemail@gmail.com', 'Antoniofree');
+            expect(obj.getGithub()).tobe('Antoniofree');
         })
     })
-});
+})});
